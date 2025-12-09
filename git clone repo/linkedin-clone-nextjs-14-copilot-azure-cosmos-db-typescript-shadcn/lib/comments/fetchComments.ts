6896e85c0c9f3cdffc3d6b65d4abe938a338afb3
Ved <ved.work2024@gmail.com>
2025-12-09 21:13:27 +0530

    fixed file structure

--- git clone repo/linkedin-clone-nextjs-14-copilot-azure-cosmos-db-typescript-shadcn/lib/comments/fetchComments.ts
+++
@@ -1,7 +0,0 @@
-const fetchComments = async (postId: string) => {
-  const response = await fetch(`/api/posts/${postId}/comments`);
-  const comments = await response.json();
-  return comments;
-};
-
-export default fetchComments;
--- git clone repo/linkedin-clone-nextjs-14-copilot-azure-cosmos-db-typescript-shadcn/types/user.ts
+++
@@ -1,6 +0,0 @@
-export interface IUser {
-  userId: string;
-  userImage: string;
-  firstName: string;
-  lastName?: string;
-}
